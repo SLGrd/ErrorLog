987969383283f6c394b676ee2a9e338cee7308ad
unknown <sylviogiraldes@yahoo.com>
2020-04-11 12:00:23 -0300

    ErrorLog - versao 0

---
+++ ErrorLog/FrmErrorLog.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace ErrorLog
+{
+    partial class FrmErrorLog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numNumerador = new System.Windows.Forms.NumericUpDown();
+            this.numDenominador = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtResultadoNum = new System.Windows.Forms.TextBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtNumerador = new System.Windows.Forms.TextBox();
+            this.txtDenominador = new System.Windows.Forms.TextBox();
+            this.txtResultadoBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnGoBox = new System.Windows.Forms.Button();
+            this.pnlTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumerador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenominador)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Controls.Add(this.label2);
+            this.pnlTop.Controls.Add(this.label1);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(796, 118);
+            this.pnlTop.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label2.Location = new System.Drawing.Point(44, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(730, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vamos provocar um ero, gravar as nformações fornecidas pelo C#  e analisar a caus" +
+    "a.";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(237, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gravação de Log de Erros";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numNumerador
+            // 
+            this.numNumerador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numNumerador.Location = new System.Drawing.Point(12, 137);
+            this.numNumerador.Name = "numNumerador";
+            this.numNumerador.Size = new System.Drawing.Size(111, 31);
+            this.numNumerador.TabIndex = 1;
+            this.numNumerador.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numNumerador.ThousandsSeparator = true;
+            // 
+            // numDenominador
+            // 
+            this.numDenominador.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numDenominador.Location = new System.Drawing.Point(258, 137);
+            this.numDenominador.Name = "numDenominador";
+            this.numDenominador.Size = new System.Drawing.Size(111, 31);
+            this.numDenominador.TabIndex = 1;
+            this.numDenominador.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(129, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "dividido por";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(396, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "igual ";
+            // 
+            // txtResultadoNum
+            // 
+            this.txtResultadoNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultadoNum.Location = new System.Drawing.Point(482, 138);
+            this.txtResultadoNum.Name = "txtResultadoNum";
+            this.txtResultadoNum.Size = new System.Drawing.Size(126, 29);
+            this.txtResultadoNum.TabIndex = 4;
+            this.txtResultadoNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGo.Location = new System.Drawing.Point(676, 132);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(88, 32);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.BtnGo_Click);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.IndianRed;
+            this.label5.Location = new System.Drawing.Point(206, 271);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(357, 54);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Vamos provocar um erro. Faça uma divisão por zero, e analise o resultado";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsg.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMsg.Location = new System.Drawing.Point(9, 337);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(780, 155);
+            this.txtMsg.TabIndex = 7;
+            // 
+            // txtNumerador
+            // 
+            this.txtNumerador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumerador.Location = new System.Drawing.Point(12, 199);
+            this.txtNumerador.Name = "txtNumerador";
+            this.txtNumerador.Size = new System.Drawing.Size(111, 29);
+            this.txtNumerador.TabIndex = 4;
+            this.txtNumerador.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtDenominador
+            // 
+            this.txtDenominador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDenominador.Location = new System.Drawing.Point(258, 199);
+            this.txtDenominador.Name = "txtDenominador";
+            this.txtDenominador.Size = new System.Drawing.Size(111, 29);
+            this.txtDenominador.TabIndex = 4;
+            this.txtDenominador.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtResultadoBox
+            // 
+            this.txtResultadoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultadoBox.Location = new System.Drawing.Point(482, 199);
+            this.txtResultadoBox.Name = "txtResultadoBox";
+            this.txtResultadoBox.Size = new System.Drawing.Size(126, 29);
+            this.txtResultadoBox.TabIndex = 4;
+            this.txtResultadoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(129, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 25);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "dividido por";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(396, 201);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 25);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "igual ";
+            // 
+            // btnGoBox
+            // 
+            this.btnGoBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGoBox.Location = new System.Drawing.Point(676, 201);
+            this.btnGoBox.Name = "btnGoBox";
+            this.btnGoBox.Size = new System.Drawing.Size(88, 32);
+            this.btnGoBox.TabIndex = 5;
+            this.btnGoBox.Text = "GO";
+            this.btnGoBox.UseVisualStyleBackColor = true;
+            this.btnGoBox.Click += new System.EventHandler(this.BtnGoBox_Click);
+            // 
+            // FrmErrorLog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(796, 500);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnGoBox);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.txtNumerador);
+            this.Controls.Add(this.txtDenominador);
+            this.Controls.Add(this.txtResultadoBox);
+            this.Controls.Add(this.txtResultadoNum);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numDenominador);
+            this.Controls.Add(this.numNumerador);
+            this.Controls.Add(this.pnlTop);
+            this.Name = "FrmErrorLog";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmErrorLog_Load);
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numNumerador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDenominador)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numNumerador;
+        private System.Windows.Forms.NumericUpDown numDenominador;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtResultadoNum;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtNumerador;
+        private System.Windows.Forms.TextBox txtDenominador;
+        private System.Windows.Forms.TextBox txtResultadoBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnGoBox;
+    }
+}
+
---
+++ ErrorLog/FrmErrorLog.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ErrorLog
+{
+    public partial class FrmErrorLog : Form
+    {
+        private readonly string ErrorLogFilePath = Application.StartupPath + "/ErrorLog.Txt";
+        readonly string CrLf = Environment.NewLine;
+
+        public FrmErrorLog() { InitializeComponent(); }
+
+        private void FrmErrorLog_Load(object sender, EventArgs e) { }
+
+        private void BtnGo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtResultadoNum.Text = Dividir( numNumerador.Value.ToString(), numDenominador.Value.ToString()).ToString("N2");
+            }
+            catch (Exception ex)
+            {
+                GravaErrorLog(ex);
+                SendTxtMsg(ex);                
+                //  Avisa o usuario
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnGoBox_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtResultadoBox.Text = Dividir( txtNumerador.Text, txtDenominador.Text).ToString("N2");
+            }
+            catch (Exception ex)
+            {
+                GravaErrorLog(ex);
+                SendTxtMsg(ex);                
+                //  Avisa o usuario
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private double Dividir(string Numerador, string Denominador)
+        {
+            double Resultado = 0;
+
+            try
+            {
+                Resultado = Double.Parse( Numerador) / Double.Parse( Denominador);
+
+                if (Double.Parse(Denominador) == 0)
+                {
+                    throw new DivideByZeroException("Denominador nao pode ser zero.", new DivideByZeroException());
+                }
+            }
+            catch ( DivideByZeroException ex)
+            {
+                GravaErrorLog(ex);
+                txtMsg.Clear();
+                SendTxtMsg(ex);
+                //  Segue com a exception devolvendo para quem chamou
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                GravaErrorLog(ex);
+                txtMsg.Clear();
+                SendTxtMsg(ex);
+                //  Segue com a exception devolvendo para quem chamou
+                throw ex;
+            }
+            finally
+            {
+                //MessageBox.Show("Fim da Rotina de Calculo");
+            }
+            return Resultado;
+        }
+
+        private void SendTxtMsg( Exception ex)
+        {
+            txtMsg.Text += "Stack Trace     = " + ex.StackTrace + CrLf;
+            txtMsg.Text += "Source          = " + ex.Source + "\r\n";
+            txtMsg.Text += "Message         = " + ex.Message + "\r\n";
+            txtMsg.Text += "Inner Exception = " + ex.InnerException + CrLf + CrLf;
+        }
+
+        private void GravaErrorLog(Exception ex)
+        {
+            //  Se o arquivo nao existir, cria e logo emseguida fecha o arquivo para que possa ser usado abaixo
+            if (!File.Exists( ErrorLogFilePath)) { File.Create( ErrorLogFilePath).Close(); }
+            
+            using (StreamWriter streamW = new StreamWriter( ErrorLogFilePath, true))
+            {
+                streamW.WriteLine(DateTime.Now + " - Stack Trace     = " + ex.StackTrace);
+                streamW.WriteLine(DateTime.Now + " - Source          = " + ex.Source);
+                streamW.WriteLine(DateTime.Now + " - Message         = " + ex.Message);
+                streamW.WriteLine(DateTime.Now + " - Inner Exception = " + ex.InnerException);
+            }
+        }
+    }
+}
